b44bf995384c72c07df560f349329b22d891983a
Giorgi <1giorgiadamia@gmail.com>
2023-10-18 21:57:22 +0400

    added swagger documentation

---
+++ src/main/java/com/example/coffeemachine/configuration/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.coffeemachine.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("AISA IT test task")
+                        .description("Coffee Machine")
+                        .version("1.0")
+                );
+    }
+}
--- src/main/java/com/example/coffeemachine/controller/CoffeeMachineController.java
+++ src/main/java/com/example/coffeemachine/controller/CoffeeMachineController.java
@@ -2,6 +2,8 @@ package com.example.coffeemachine.controller;
 
 import com.example.coffeemachine.entity.CoffeeMachine;
 import com.example.coffeemachine.service.CoffeeMachineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,47 +14,55 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/coffee-machines")
 @RequiredArgsConstructor
+@Tag(name = "Coffee Machine Controller", description = "Coffee Machine API")
 public class CoffeeMachineController {
 
     private final CoffeeMachineService coffeeMachineService;
 
     @PostMapping("/create")
+    @Operation(summary = "Create a coffee machine")
     public ResponseEntity<Void> createCoffeeMachine(@RequestBody CoffeeMachine coffeeMachine) {
         coffeeMachineService.createCoffeeMachine(coffeeMachine);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Display all coffee machines")
     public ResponseEntity<List<CoffeeMachine>> getAllCoffeeMachines() {
         List<CoffeeMachine> coffeeMachines = coffeeMachineService.getAllCoffeeMachines();
         return new ResponseEntity<>(coffeeMachines, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a coffee machine")
     public ResponseEntity<Void> deleteCoffeeMachine(@PathVariable Long id) {
         coffeeMachineService.deleteCoffeeMachine(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/turn-on")
+    @Operation(summary = "Turn on a coffee machine")
     public ResponseEntity<Void> turnOnCoffeeMachine(@PathVariable Long id) {
         coffeeMachineService.turnOnCoffeeMachine(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/turn-off")
+    @Operation(summary = "Turn off a coffee machine")
     public ResponseEntity<Void> turnOffCoffeeMachine(@PathVariable Long id) {
         coffeeMachineService.turnOffCoffeeMachine(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/pour-coffee")
+    @Operation(summary = "Pour coffee from a coffee machine")
     public ResponseEntity<Void> pourCoffee(@PathVariable Long id, @RequestParam Integer quantity) {
         coffeeMachineService.pourCoffee(id, quantity);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping("/{id}/add-coffee")
+    @Operation(summary = "Add coffee to a coffee machine")
     public ResponseEntity<Void> addCoffee(@PathVariable Long id, @RequestParam Integer quantity) {
         coffeeMachineService.addCoffee(id, quantity);
         return new ResponseEntity<>(HttpStatus.OK);
